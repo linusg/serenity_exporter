6a5ee0ae2be62c6efd270093ee2ca6ee73891846
Linus Groh <mail@linusgroh.de>
2022-06-06 19:04:10 +0100

    Add declaration for loadINI()

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,6 @@
+type INIObject = {
+    [group: string]: { [key: string]: string };
+};
+
+declare function loadINI(path: string): INIObject;
 declare function loadJSON(path: string): ReturnType<typeof JSON.parse>;
